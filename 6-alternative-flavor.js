08cb2f3d0e7e219ea66e4d698503d74d2270cc57
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2025-05-18 22:49:24 +0500

    alternative way to export module

---
+++ 6-alternative-flavor.js
@@ -0,0 +1,7 @@
+module.exports.items = ['item 1', 'item 2'];
+
+const person = {
+    name: 'baby'
+}
+
+module.exports.singlePerson = person;
--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 
 const names = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor')
 
-//console.log(sayHi);
+console.log(data);
 
 sayHi('Susan')
 sayHi(names.john)
