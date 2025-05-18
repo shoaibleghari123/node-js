ece441a8dde6b70ef05c39312712108ee3c734c4
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2025-05-18 19:35:13 +0500

    modules and exports

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-console.log(__dirname)
+//CommonJS, every file is module(by default)
+//Modules - Encapsulated Code (only share minimum)
 
-setInterval(() => {
-    console.log("hello world");
-}, 1000)
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+
+//console.log(sayHi);
+
+sayHi('Susan')
+sayHi(names.john)
+sayHi(names.peter)
